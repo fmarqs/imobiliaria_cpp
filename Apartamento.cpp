0042585e38839ae5378e86a108d258268533f30a
fmarqs <50125854+fmarqs@users.noreply.github.com>
2019-08-17 00:53:26 -0300

    Mudanças no cadastro

--- Apartamento.cpp
+++ Apartamento.cpp
@@ -1,6 +1,7 @@
 #include "Apartamento.h"
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -9,14 +10,60 @@ Apartamento::Apartamento()
     
 }
 
+void Apartamento::cadastraApt()
+{
+    double ar, cond;
+    int qua, an, gar;
+    string pos;
+
+    cout << "\nArea: ";
+    cin >> ar;
+    area = ar;
+    cout << "\nValor do condominio: ";
+    cin >> cond;
+    condominio = cond;
+    cout << "\nQuartos: ";
+    cin >> qua;
+    quartos = qua;
+    cout << "\nAndar: ";
+    cin >> an;
+    andar = an;
+    cout << "\nGaragem: ";
+    cin >> gar;
+    garagem = gar;
+    cin.ignore();
+    cout << "\nPosicao: "; 
+    getline(cin, pos);
+    
+
+    setApartamento(ar, cond, qua, an, gar, pos);
+
+}
+
+void Apartamento::setApartamento(double ar, double cond, int qua, int an, int gar, std::string pos)
+{
+    area = ar;
+    condominio = cond;
+    quartos = qua;
+    andar = an;
+    garagem = gar;
+    posicao = pos;
+}
+
+
+string Apartamento::toStringApt()
+{
+    stringstream apart;
+
+    apart << area << endl << condominio << endl << quartos << endl << andar << endl << garagem << endl << posicao << endl;
+
+
+    return apart.str();
+}
+
 double Apartamento::getArea()
 {
     return area;
-}
-
-int Apartamento::getAndar()
-{
-    return andar;
 }
 
 double Apartamento::getCondominio()
@@ -24,9 +71,9 @@ double Apartamento::getCondominio()
     return condominio;
 }
 
-int Apartamento::getQuartos()
+int Apartamento::getAndar()
 {
-    return quartos;
+    return andar;
 }
 
 int Apartamento::getGaragem()
@@ -34,60 +81,12 @@ int Apartamento::getGaragem()
     return garagem;
 }
 
+int Apartamento::getQuartos()
+{
+    return quartos;
+}
+
 string Apartamento::getPosicao()
 {
     return posicao;
-}
-
-void Apartamento::setArea()
-{
-    double a;
-    cout << "Area: ";
-    cin >> a;
-    area = a;
-}
-
-void Apartamento::setCondominio()
-{
-    double cond;
-    cout << "Valor do condominio: ";
-    cin >> cond;
-    condominio = cond;
-}
-
-void Apartamento::setQuartos()
-{
-    int qua;
-    cout << "Quartos: ";
-    cin >> qua;
-    quartos = qua;
-}
-
-void Apartamento::setAndar()
-{
-    int seila;
-    cout << "Andar: ";
-    cin >> seila;
-    andar = seila;
-}
-
-void Apartamento::setGaragem()
-{
-    int garag;
-    cout << "Vagas na garagem: ";
-    cin >> garag;
-    garagem = garag;
-}
-
-void Apartamento::setPosicao()
-{
-    string pos;
-    cout << "Posicao: ";
-    getline(cin, pos);
-    posicao = pos;
-}
-
-void Apartamento::print()
-{
-    
 }

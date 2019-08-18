59de50c4a5962fd989386f2d983860d9873f3ec1
fmarqs <50125854+fmarqs@users.noreply.github.com>
2019-08-17 23:09:00 -0300

    seila

--- Imobiliaria.cpp
+++ Imobiliaria.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <fstream>
 
-
 using namespace std;
 
 Imobiliaria::Imobiliaria()
@@ -33,6 +32,7 @@ void Imobiliaria::cadastroCasa(Casas obj)
     obj.cadastraEndereco();
     obj.cadastraCasa();
     casas.push_back(obj);
+    setCasas(1);
 }
 
 void Imobiliaria::cadastroApartamento(Apartamento obj)
@@ -42,6 +42,7 @@ void Imobiliaria::cadastroApartamento(Apartamento obj)
     obj.cadastraEndereco();
     obj.cadastraApt();
     apt.push_back(obj);
+    setApt(1);
 }
 
 void Imobiliaria::cadastroTerreno(Terreno obj)
@@ -50,25 +51,62 @@ void Imobiliaria::cadastroTerreno(Terreno obj)
     obj.cadastraEndereco();
     obj.cadastraTerreno();
     ter.push_back(obj);
-
+    setTer(1);
 }
 
 void Imobiliaria::printCasas()
 {
-    for(int i=0; i<casas.size(); i++)
-        cout << casas[i] << endl;
+
+    for(int i=0; i<n_casas; i++)
+    {
+        cout << "Tipo: " << casas[i].getTipo() << endl;
+        cout << "Valor: " << casas[i].getValor() << endl;
+        cout << "Rua: " << casas[i].getRua() << endl;
+        cout << "Numero: " << casas[i].getNumero() << endl;
+        cout << "Bairro: " << casas[i].getBairro() << endl;
+        cout << "Cidade: " << casas[i].getCidade() << endl;
+        cout << "CEP: " << casas[i].getCep() << endl;
+        cout << "Pavimentos: " << casas[i].getPavimentos() << endl;
+        cout << "Quartos: " << casas[i].getQuartos() << endl;
+        cout << "Area construida: " << casas[i].getAreaC() << endl;
+        cout << "Area do terreno: " << casas[i].getAreaT() << endl;
+
+    }
 }
 
 void Imobiliaria::printApartamento()
 {
-    for(int i=0; i<apt.size(); i++)
-        cout << apt[i] << endl;
+    
+    for(int i=0; i<n_apt; i++)
+    {
+        cout << "Tipo: " << apt[i].getTipo() << endl;
+        cout << "Valor: " << apt[i].getValor() << endl;
+        cout << "Rua: " << apt[i].getRua() << endl;
+        cout << "Numero: " << apt[i].getNumero() << endl;
+        cout << "Bairro: " << apt[i].getBairro() << endl;
+        cout << "Cidade: " << apt[i].getCidade() << endl;
+        cout << "CEP: " << apt[i].getCep() << endl;
+        cout << "Area: " << apt[i].getArea() << endl;
+        cout << "Condominio: " << apt[i].getCondominio() << endl;
+        cout << "Quartos: " << apt[i].getQuartos() << endl;
+        cout << "Andar: " << apt[i].getAndar() << endl;
+        cout << "Garagem: " << apt[i].getGaragem() << endl;
+        cout << "Posicao: " << apt[i].getPosicao() << endl;
+    }
 }
 
 void Imobiliaria::printTerreno()
 {
-    for(int i=0; i<ter.size(); i++)
-        cout << ter[i] << endl;
+    for(int i=0; i<n_ter; i++)
+    {
+        cout << "Valor: " << ter[i].getPreco() << endl;
+        cout << "Rua: " << ter[i].getRua() << endl;
+        cout << "Numero: " << ter[i].getNumero() << endl;
+        cout << "Bairro: " << ter[i].getBairro() << endl;
+        cout << "Cidade: " << ter[i].getCidade() << endl;
+        cout << "CEP: " << ter[i].getCep() << endl;
+        cout << "Area: " << ter[i].getArea() << endl;
+    }
 
 }
 
@@ -84,15 +122,15 @@ int Imobiliaria::lerDados()
          imov.close();
          return 0;
      }
-    
+
     imov >> n_casas;
     imov >> n_apt;
     imov >> n_ter;
     imov.close();
 
     string b, ci, ce, r;
-    bool tipo;
-    double val; 
+    int tipo;
+    double val;
     int num;
 
     for(int i=0; i<n_casas; i++)
@@ -116,7 +154,7 @@ int Imobiliaria::lerDados()
         lista >> areat;
         lista.ignore();
         Casas obj = Casas();
-        obj.setCasa(tipo,val, r, num, b, ci, ce, pav, q, areac, areat); //mudar isso aq
+        obj.lerCasas(tipo,val, r, num, b, ci, ce, pav, q, areac, areat);
         casas.push_back(obj);
     }
 
@@ -128,8 +166,8 @@ int Imobiliaria::lerDados()
 
         if(lista.eof())
             return 0;
-        
-        getline(lista, tipo);
+
+        lista >> tipo;
         lista >> val;
         lista.ignore();
         getline(lista, r);
@@ -146,7 +184,7 @@ int Imobiliaria::lerDados()
         lista.ignore();
         getline(lista, pos);
         Apartamento obj = Apartamento();
-        obj.setApartamento(area, cond, q, a, vagas, pos);
+        obj.lerApt(tipo, val, r, num, b, ci, ce, area, cond, q, a, vagas, pos);
         apt.push_back(obj);
 
     }
@@ -169,12 +207,12 @@ int Imobiliaria::lerDados()
         lista >> ar;
         lista.ignore();
         Terreno obj = Terreno();
-        obj.setTerreno(ar, pre);
+        obj.lerTerreno(pre, r, num, b, ci, ce, ar);
         ter.push_back(obj);
 
     }
-    
-     
+
+
 }
 
 int Imobiliaria::salvarDados()
@@ -199,31 +237,43 @@ int Imobiliaria::salvarDados()
     {
         lista << casas[i].getTipo() << endl;
         lista << casas[i].getValor() << endl;
-        lista << casas[i].getEndereco() << endl;
+        lista << casas[i].getRua() << endl;
+        lista << casas[i].getBairro() << endl;
+        lista << casas[i].getNumero() << endl;
+        lista << casas[i].getCidade() << endl;
+        lista << casas[i].getCep() << endl;
         lista << casas[i].getPavimentos() << endl;
         lista << casas[i].getQuartos() << endl;
         lista << casas[i].getAreaC() << endl;
-        lista << casas[i].getAreaT() << "\n" << endl;
+        lista << casas[i].getAreaT() << endl;
     }
 
     for(int i=0; i<n_apt; i++)
     {
         lista << apt[i].getTipo() << endl;
         lista << apt[i].getValor() << endl;
-        lista << apt[i].getEndereco() << endl;
+        lista << apt[i].getRua() << endl;
+        lista << apt[i].getBairro() << endl;
+        lista << apt[i].getNumero() << endl;
+        lista << apt[i].getCidade() << endl;
+        lista << apt[i].getCep() << endl;
         lista << apt[i].getArea() << endl;
         lista << apt[i].getCondominio() << endl;
         lista << apt[i].getQuartos() << endl;
         lista << apt[i].getAndar() << endl;
         lista << apt[i].getGaragem() << endl;
-        lista << apt[i].getPosicao() << "\n" << endl;
+        lista << apt[i].getPosicao() << endl;
     }
 
     for(int i=0; i<n_ter; i++)
     {
         lista << ter[i].getPreco() << endl;
-        lista << ter[i].getEndereco() << endl;
-        lista << ter[i].getArea() << "\n" << endl;
+        lista << ter[i].getRua() << endl;
+        lista << ter[i].getBairro() << endl;
+        lista << ter[i].getNumero() << endl;
+        lista << ter[i].getCidade() << endl;
+        lista << ter[i].getCep() << endl;
+        lista << ter[i].getArea() << endl;
     }
 
     lista.close();
